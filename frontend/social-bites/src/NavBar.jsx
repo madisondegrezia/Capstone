29f406f7a86ca0bf669186da5c7fc92204bdf789
Samr Mouna <samr.mouna@gmail.com>
2023-07-26 14:58:45 -0400

    fixed return value in navbar component

--- frontend/social-bites/src/NavBar.jsx
+++ frontend/social-bites/src/NavBar.jsx
@@ -5,16 +5,15 @@ export default function NavBar() {
 
 
     return (
-        <>
+        <div className="entire-page">
         <div className="headerBar">
             <img className="logo" src="/src/assets/socialbites-logo.png" alt="logo"></img>
             <div className="right">
-                
                 <button className="login">Log In</button>
                 <img className="user-picture" src="/src/assets/default-avatar.webp" alt="user picture"></img>
             </div>
         </div>
         <Outlet />
-        </>
+        </div>
     )
 }
