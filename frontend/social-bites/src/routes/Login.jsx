e6a00d6fd0f8a73251bf5b6c8482cc55d5a75e2e
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-07-27 14:47:14 -0400

    login and signup page styling

--- frontend/social-bites/src/routes/Login.jsx
+++ frontend/social-bites/src/routes/Login.jsx
@@ -1,40 +1,51 @@
 import { Form } from "react-router-dom";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import "./Login.css";
 
 function Login() {
   return (
-    <Form method="post" className="selection:bg-red-200 flex flex-col gap-2 flex items-center">
-      <div className="border-solid border-2 border-red-500 rounded-xl p-10 mt-8 shadow-2xl bg-red-400">
-      <h1 className="text-3xl mt-5 text-center flex flex-row items-center justify-center">Login <FaUser /></h1>
+    <div className="img h-screen flex items-center justify-center">
+      <Form
+        method="post"
+        className="selection:bg-red-200 flex flex-col gap-2 flex items-center"
+      >
+        <div className="border-solid rounded-xl p-10 mt-8 shadow-2xl background-color">
+          <h1 className="text-3xl mt-5 text-center flex flex-row items-center justify-center">
+            Login <FaUser />
+          </h1>
 
-      <fieldset className="flex flex-col">
-        <label htmlFor="title">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          className="border-2 rounded-md bg-red-100 focus:outline-none p-2"
-        />
-      </fieldset>
-      <fieldset className="flex flex-col">
-        <label htmlFor="company">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          className="border-2 rounded-md bg-red-100 focus:outline-none p-2"
-        />
-      </fieldset>
-      <div className="text-center">
-      <input
-        className="bg-red-700 hover:bg-red-800 text-white rounded-md transition mt-4 py-2 cursor-pointer w-32"
-        type="submit" value="Login"
-      ></input>
-      </div>
-      <Link to="/signup" className="flex mr-3 mt-3 underline">Do not have an account?</Link>
+          <fieldset className="flex flex-col">
+            <label htmlFor="title">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="border-2 rounded-md bg-red-100 focus:outline-none p-2"
+            />
+          </fieldset>
+          <fieldset className="flex flex-col">
+            <label htmlFor="company">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="border-2 rounded-md bg-red-100 focus:outline-none p-2"
+            />
+          </fieldset>
+          <div className="text-center">
+            <input
+              className="bg-red-700 hover:bg-red-800 text-white rounded-md transition mt-4 py-2 cursor-pointer w-32"
+              type="submit"
+              value="Login"
+            ></input>
+          </div>
+          <Link to="/signup" className="flex mr-3 mt-3 underline text-white">
+            Do not have an account?
+          </Link>
+        </div>
+      </Form>
     </div>
-    </Form>
   );
 }
 
