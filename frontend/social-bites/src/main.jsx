f316affe57aa7b8f6da4020cdaa0cd110e7d2846
Samr Mouna <samr.mouna@gmail.com>
2023-07-27 11:49:39 -0400

    added loader

--- frontend/social-bites/src/main.jsx
+++ frontend/social-bites/src/main.jsx
@@ -5,16 +5,19 @@ import "./index.css";
 import ErrorPage from "./ErrorPage";
 import NavBar from "./NavBar/NavBar";
 import Home from "./Home/Home";
+import load from "./Loader/loadRestaurants";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    loader: load,
     element: (
       <NavBar />
     ),
     children: [
       {
         path: "/",
+        loader: load,
         element: (
           <Home />
         )
