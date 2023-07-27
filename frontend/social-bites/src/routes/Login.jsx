f0686056375c7a26b1c17f6c7b871c31b800ed60
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-07-27 11:10:30 -0400

    login page styling

--- frontend/social-bites/src/routes/Login.jsx
+++ frontend/social-bites/src/routes/Login.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <Form method="post" className="selection:bg-red-200 flex flex-col gap-2">
-      <h1 className="text-3xl mt-4 text-center flex flex-row items-center justify-center">Login <FaUser /></h1>
+    <Form method="post" className="selection:bg-red-200 flex flex-col gap-2 flex items-center">
+      <div className="border-solid border-2 border-red-500 rounded-xl p-10 mt-8 shadow-2xl">
+      <h1 className="text-3xl mt-5 text-center flex flex-row items-center justify-center">Login <FaUser /></h1>
 
       <fieldset className="flex flex-col">
         <label htmlFor="title">Email</label>
@@ -25,11 +26,14 @@ function Login() {
           className="border-4 focus:outline-none p-2"
         />
       </fieldset>
+      <div className="text-center">
       <input
-        className="bg-red-500 hover:bg-red-600 text-white transition mt-4 py-2 cursor-pointer"
-        type="submit"
+        className="bg-red-500 hover:bg-red-600 text-white transition mt-4 py-2 cursor-pointer w-32"
+        type="submit" value="Login"
       ></input>
-      <Link to="/signup" className="flex flex-row-reverse mr-3 underline">Do not already have an account?</Link>
+      </div>
+      <Link to="/signup" className="flex mr-3 mt-3 underline">Do not have an account?</Link>
+    </div>
     </Form>
   );
 }
