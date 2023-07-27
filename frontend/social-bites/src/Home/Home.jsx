1a646817e2105cec6ca57f77e82e748b8561855b
Samr Mouna <samr.mouna@gmail.com>
2023-07-27 11:49:15 -0400

    editing contents of home to include hero image and restaurant cards

--- frontend/social-bites/src/Home/Home.jsx
+++ frontend/social-bites/src/Home/Home.jsx
@@ -1,15 +1,17 @@
 import { Form } from "react-router-dom"
 import "./HomeStyle.css"
+import RestaurantCards from "../RestaurantCards/RestaurantCards"
 
 export default function Home() {
   return (
     <div className="home-page">
+      <img className="head-img" src="https://media.bizj.us/view/img/12359290/df104-5-of-13*1200xx2500-1406-0-255.jpg"></img>
       <Form method="get" className="search-bar">
       <input id="search-res" className="border-2" placeholder="Search For Restaurants..."/>
       </Form>
 
-      <div className="restaraunt-cards">
-        cards go here!
+      <div className="restaurant-cards">
+        <RestaurantCards />
       </div>
     </div>
   )
