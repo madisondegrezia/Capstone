6b011d503f9023a1a9ffccb7da3680fb65e84f32
Vitali <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-27 13:42:28 -0400

    nav bar links

--- frontend/social-bites/src/NavBar/NavBar.jsx
+++ frontend/social-bites/src/NavBar/NavBar.jsx
@@ -1,21 +1,47 @@
-import { useState } from "react"
-import { Outlet, Link } from "react-router-dom/dist/umd/react-router-dom.development"
-import "./NavBarStyle.css"
+import { useState } from "react";
+import {
+  Outlet,
+  Link,
+} from "react-router-dom/dist/umd/react-router-dom.development";
+import "./NavBarStyle.css";
 
 export default function NavBar() {
-
-
-    return (
-        <div className="entire-page">
-        <div className="headerBar">
-            <Link to="/" className="flex h-full"><img className="logo" src="/src/assets/socialbites-logo.png" alt="logo" /></Link>
-            <div className="right">
-                {/* <button className="login">Log In</button> */}
-                <Link to="/login" className="login">Log in</Link>
-                <img className="user-picture" src="/src/assets/default-avatar.webp" alt="user picture"></img>
-            </div>
+  return (
+    <div className="entire-page">
+      <div className="headerBar">
+        <Link to="/" className="flex h-full">
+          <img
+            className="logo"
+            src="/src/assets/socialbites-logo.png"
+            alt="logo"
+          />
+        </Link>
+        <div className="nav-menu">
+          <ul className="nav-menu-list">
+            <li className="nav-menu-item">
+              <Link to="/about">About</Link>
+            </li>
+            <li className="nav-menu-item">
+              <Link to="/faq">FAQ</Link>
+            </li>
+            <li className="nav-menu-item">
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
         </div>
-        <Outlet />
+        <div className="right">
+          {/* <button className="login">Log In</button> */}
+          <Link to="/login" className="login">
+            Log in
+          </Link>
+          <img
+            className="user-picture"
+            src="/src/assets/default-avatar.webp"
+            alt="user picture"
+          ></img>
         </div>
-    )
+      </div>
+      <Outlet />
+    </div>
+  );
 }
