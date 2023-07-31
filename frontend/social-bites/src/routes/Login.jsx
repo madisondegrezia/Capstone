bcc0ccb4527fe8d81262efaf0b69498e003cd0cb
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-07-31 10:41:42 -0400

    changes made to login

--- frontend/social-bites/src/routes/Login.jsx
+++ frontend/social-bites/src/routes/Login.jsx
@@ -1,19 +1,37 @@
-import { Form } from "react-router-dom";
+import { Form, Navigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const { currentUser, login, authError } = useContext(AuthContext);
+
+  if (currentUser) {
+    return <Navigate to="/" />;
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const credentials = Object.fromEntries(formData);
+    await login(credentials);
+  };
+
   return (
     <div className="img-login h-screen flex items-center justify-center">
       <Form
-        method="post"
+        onSubmit={handleSubmit}
         className="selection:bg-red-200 flex flex-col gap-2 flex items-center"
       >
         <div className="border-solid rounded-xl p-10 mt-8 shadow-2xl background-color">
           <h1 className="text-3xl mt-5 text-center flex flex-row items-center justify-center">
             Login <FaUser />
           </h1>
+
+          {authError && <div className="text-red-500">{authError}</div>}
+
           <fieldset className="flex flex-col">
             <label htmlFor="title">Email</label>
             <input
