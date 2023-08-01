fde0e9269af36f2342dc303f776eb5134e264a57
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-08-01 15:12:23 -0400

    additional image for slider

--- frontend/social-bites/src/Slider/Slider.jsx
+++ frontend/social-bites/src/Slider/Slider.jsx
@@ -21,6 +21,9 @@ function App() {
     {
       url: "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
     },
+    {
+      url: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
