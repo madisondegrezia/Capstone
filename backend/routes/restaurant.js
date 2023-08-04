55836e04193ec2091f447b9d7ec9894c247f7daf
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-08-04 14:32:27 -0400

    loading current user data

--- backend/routes/restaurant.js
+++ backend/routes/restaurant.js
@@ -4,154 +4,162 @@ const { Restaurant } = require("../models");
 const { autheticateUser } = require("../middleware/authUser");
 require("dotenv").config();
 
-
 // import axios from the axios to fetch for google map api
 const axios = require("axios");
 const googleApiKey = process.env.GOOGLE_API_KEY;
 
 // get restaurant by their by id
-router.get("/:restaurantId", async (req, res)=>{
-    const restaurantId = parseInt(req.params.restaurantId,10);
+router.get("/:restaurantId", async (req, res) => {
+  const restaurantId = parseInt(req.params.restaurantId, 10);
 
-    try{
-        const restaurant = await Restaurant.findOne({ where: {id: restaurantId} });
+  try {
+    const restaurant = await Restaurant.findOne({
+      where: { id: restaurantId },
+    });
 
     if (!restaurant) {
-      return res.status(404).json({ message: "There are no restaurant found!" });
+      return res
+        .status(404)
+        .json({ message: "There are no restaurant found!" });
     }
     return res.status(200).json(restaurant);
-
-    } catch(error){
-        console.error(error);
-    return res.status(500).json({ 
-        message: "An error occured during fetching restaurant" 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error occured during fetching restaurant",
     });
-    }
+  }
 });
 
 // delete destaurant from the database
-router.delete("/:restaurantId", async (req, res)=>{
-    const restaurantId = parseInt(req.params.restaurantId,10);
+router.delete("/:restaurantId", async (req, res) => {
+  const restaurantId = parseInt(req.params.restaurantId, 10);
 
-    try{
-        const restaurant = await Restaurant.destroy({ where: {id: restaurantId} });
+  try {
+    const restaurant = await Restaurant.destroy({
+      where: { id: restaurantId },
+    });
 
     if (!restaurant) {
-      return res.status(404).json({ message: "There are no restaurant found!" });
+      return res
+        .status(404)
+        .json({ message: "There are no restaurant found!" });
     }
-    return res.status(200).json({message: "The restaurant is deleted successfully"});
-
-    } catch(error){
-        console.error(error);
-    return res.status(500).json({ 
-        message: "An error occured during fetching restaurant" 
+    return res
+      .status(200)
+      .json({ message: "The restaurant is deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error occured during fetching restaurant",
     });
-    }
+  }
 });
 
-
 // fetch to google map api for the address's lat and lng by axios
-async function fetchRestaurantLatLng(address){
-    try{
-        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${googleApiKey}`);
-        if (response){
-            const result = response.data.results[0];
-            const latitude = result.geometry.location.lat;
-            const longitude = result.geometry.location.lng;
-            return {latitude: latitude, longitude: longitude};
-        }
-        else {
-            return false;
-        }
-    } catch(error){
-        console.error("Error:", error.message);
+async function fetchRestaurantLatLng(address) {
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+        address
+      )}&key=${googleApiKey}`
+    );
+    if (response) {
+      const result = response.data.results[0];
+      const latitude = result.geometry.location.lat;
+      const longitude = result.geometry.location.lng;
+      return { latitude: latitude, longitude: longitude };
+    } else {
+      return false;
     }
-
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
 }
 
 // update the restaurant info, need log in
-router.patch("/editRestaurant/:restaurantId", autheticateUser, async(req, res)=>{
+router.patch(
+  "/editRestaurant/:restaurantId",
+  autheticateUser,
+  async (req, res) => {
     const restaurantId = parseInt(req.params.restaurantId, 10);
     let latLng = null;
 
     // fetch for restaurant in the db
     const restaurantExist = await Restaurant.findOne({
-        UserId: parseInt(req.session.userId, 10),
-        RestaurantId: restaurantId
+      UserId: parseInt(req.session.userId, 10),
+      RestaurantId: restaurantId,
     });
 
-    if (!restaurantExist){
-        return res.status(404).json({message: "Restaurant Not Found"});
+    if (!restaurantExist) {
+      return res.status(404).json({ message: "Restaurant Not Found" });
     }
 
     // if restaurant does exist, check if the user need to change address
     // if need to change address, fetch for a new latitude and longitude from google geolocation api
-    if (req.body.address !== null){
-        latLng = await fetchRestaurantLatLng(req.body.address);
+    if (req.body.address !== null) {
+      latLng = await fetchRestaurantLatLng(req.body.address);
     }
 
     // update the restaurant info
     await restaurantExist.update({
-        restaurantName: (req.body.restaurantName)? req.body.restaurantName : restaurantExist.restaurantName,
-        address: (latLng)? req.body.address : restaurantExist.address,
-        latitude: (latLng)? latLng.latitude : restaurantExist.latitude,
-        longitude: (latLng)? latLng.longitude : restaurantExist.longitude
+      restaurantName: req.body.restaurantName
+        ? req.body.restaurantName
+        : restaurantExist.restaurantName,
+      address: latLng ? req.body.address : restaurantExist.address,
+      latitude: latLng ? latLng.latitude : restaurantExist.latitude,
+      longitude: latLng ? latLng.longitude : restaurantExist.longitude,
     });
 
-    return res.status(200).json({message: "It was updated successfully"});
-
-});
+    return res.status(200).json({ message: "It was updated successfully" });
+  }
+);
 
 // post a restauarant, require user login
-router.post("/", autheticateUser,async (req, res)=>{
+router.post("/", autheticateUser, async (req, res) => {
+  // find the latitude and longitude of the restaurant based on the address given
+  const latLng = await fetchRestaurantLatLng(req.body.address);
+  console.log(latLng);
 
-    // find the latitude and longitude of the restaurant based on the address given
-    const latLng = await fetchRestaurantLatLng(req.body.address);
-
-    try{
-        const restaurant = await Restaurant.create({
-            UserId: req.session.userId,
-            restaurantName: req.body.restaurantName,
-            address: req.body.address,
-            latitude: latLng.latitude,
-            longitude: latLng.longitude
-        });
-
-        return res.status(201).json({
-            message: "The restaurant is created successfully",
-            restaurant: {
-                restaurant: restaurant.restaurantName
-            }
-        });
-
-
-    } catch(error){
-        console.error(error);
-    return res.status(500).json({ 
-        message: "An error occured during creating restaurant" // the error include wrong address that can't be fetched by google map api
+  try {
+    const restaurant = await Restaurant.create({
+      UserId: req.session.userId,
+      restaurantName: req.body.restaurantName,
+      address: req.body.address,
+      latitude: latLng.latitude,
+      longitude: latLng.longitude,
     });
-    }
+
+    return res.status(201).json({
+      message: "The restaurant is created successfully",
+      restaurant: {
+        restaurant: restaurant.restaurantName,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error occured during creating restaurant", // the error include wrong address that can't be fetched by google map api
+    });
+  }
 });
 
 // get all restaurants
-router.get("/", async (req, res)=>{
-    try{
-        const restaurants = await Restaurant.findAll();
+router.get("/", async (req, res) => {
+  try {
+    const restaurants = await Restaurant.findAll();
 
     if (restaurants.length === 0) {
       return res.status(404).json({ message: "There are no restaurants!" });
     }
 
     return res.status(200).json(restaurants);
-
-    } catch(error){
-        console.error(error);
-    return res.status(500).json({ 
-        message: "An error occured during fetching restaurants" 
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "An error occured during fetching restaurants",
     });
-    }
+  }
 });
-
-
 
 module.exports = router;
