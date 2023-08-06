8a2b40db1beaef10300924bd39977d7e9d1a2936
Vitali <vitaliy.prymak@cix.csi.cuny.edu>
2023-08-06 16:34:59 -0400

    need help to fix action for add restaurant

--- frontend/social-bites/src/RestaurantSettings/AddRestaurant/AddRestaurant.jsx
+++ frontend/social-bites/src/RestaurantSettings/AddRestaurant/AddRestaurant.jsx
@@ -1,17 +1,10 @@
-import { useActionData } from "react-router-dom/dist/umd/react-router-dom.development";
 import "./AddRestaurant.css";
 import { Form, redirect } from "react-router-dom";
 
 export default function AddRestaurant() {
-  const data = useActionData();
-
   return (
     <div className="add-restaurant">
-      <Form
-        className="profile"
-        method="post"
-        action="/restaurant/settings/addrestaurant"
-      >
+      <Form className="profile" method="post" action="/restaurant/settings/add">
         <h1 className="title">Add Restaurant</h1>
         <h2 className="sub-title">Restaurant Name</h2>
         <input
@@ -36,13 +29,45 @@ export default function AddRestaurant() {
   );
 }
 
+// export async function action({ request, params }) {
+//   let formData = await request.formData();
+//   let jobData = Object.fromEntries(formData);
+//   jobData.minSalary = parseInt(jobData.minSalary);
+//   jobData.maxSalary = parseInt(jobData.maxSalary);
+//   const response = await fetch("http://localhost:4000/api/restaurant", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(jobData),
+//   });
+//   return redirect("/restaurant/settings");
+// }
+
 export const addRestaurantAction = async ({ request }) => {
   const data = await request.formData();
 
-  const submission = {
-    name: data.get("name"),
-    address: data.get("address"),
-  };
+  const response = await fetch("http://localhost:4000/api/restaurant", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: {
+      restaurantName: data.get("name"),
+      address: data.get("address"),
+    },
+  });
 
   return redirect("/restaurant/settings/");
 };
+
+// export const addRestaurantAction = async ({ request }) => {
+//   const data = await request.formData();
+
+//   const submission = {
+//     restaurantName: data.get("name"),
+//     address: data.get("address"),
+//   };
+
+//   return redirect("/restaurant/settings/");
+// };
--- frontend/social-bites/src/RestaurantSettings/EditRestaurant/EditRestaurant.jsx
+++ frontend/social-bites/src/RestaurantSettings/EditRestaurant/EditRestaurant.jsx
@@ -1,5 +1,34 @@
 import "./EditRestaurant.css";
 
+import { Form } from "react-router-dom";
 export default function EditRestaurant() {
-  return <div className="edit-restaurant">Edit Restaurant</div>;
+  return (
+    <div className="edit-restaurant">
+      <Form
+        className="profile"
+        method="post"
+        action="/restaurant/settings/addrestaurant"
+      >
+        <h1 className="title">Edit Restaurant</h1>
+        <h2 className="sub-title">Restaurant Name</h2>
+        <input
+          name="name"
+          className="input"
+          type="text"
+          placeholder="Lord of the Fries"
+        />
+        <h2 className="sub-title">Restaurant Address</h2>
+        <input
+          name="address"
+          className="input"
+          type="text"
+          placeholder="1234 Laughter Lane, Giggle City, SMILE 56789"
+        />
+
+        <button type="submit" className="btn">
+          Update
+        </button>
+      </Form>
+    </div>
+  );
 }
