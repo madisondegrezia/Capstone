45e93e5302aaf6c026372b915b5f64e4a8cef037
Samr Mouna <samr.mouna@gmail.com>
2023-08-06 03:19:45 -0400

    finalizing styles before adding functionality

--- frontend/social-bites/src/UserPage/UserPage.jsx
+++ frontend/social-bites/src/UserPage/UserPage.jsx
@@ -35,22 +35,76 @@ export default function UserPage() {
                             <Link to="" className="user-menu-item flex flex-row"><PiForkKnifeBold />Following</Link>
                         </li>
                     </ul>
-
+                    
                     <div className="break"/>
                     <p className="report-button">Report this Profile?</p>
                 </div>
-                <div className="break-right" />
-                <div className="profile-reviews flex flex-col items-center">
+                <div className="containing">
+                    <div className="break-right" />
+                </div>
+                <div className="profile-reviews flex flex-col items-start p-7 pt-1">
+                    <h1 className="review-title">Reviews</h1>
                     <p>Sort by: Options here</p>
                     <div className="review-boxes">
                         <div className="review-box">
-                            <img className="user-image-small w-14 h-14" src="./src/assets/default-avatar.webp"></img>
+                            <div className="review-header flex flex-row justify-between">
+                                <div className="res-detail-box flex flex-row gap-4">
+                                <img className="user-image-small w-14 h-14" src="./src/assets/default-avatar.webp"></img>
+                                    <div className="res-details">
+                                    <p>McDoolans</p>
+                                    <p>1234 Street Street</p>
+                                    </div>
+                                </div>
+                                <div className="rate flex flex-row">
+                                <FaStar size={30}style={{color: "orange"}}/><FaStar size={30}style={{color: "orange"}}/><FaStar size={30}style={{color: "orange"}}/>
+                                </div>
+                            </div>
+                            <p className="review-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div className="break-right" />
+                <div className="containing">
+                    <div className="break-right" />
+                </div>
                 <div className="profile-right">
-
+                    <h1 className="review-title p-7 pt-0 pb-0">About</h1>
+                    <ul className="p-8">
+                        <li>
+                            <h2>Location</h2>
+                            <p>New York</p>
+                        </li>
+                        <li>
+                            <h2>Favorite Food</h2>
+                            <p>Sushi</p>
+                        </li>
+                        <li>
+                            <h2>Favorite Restaurant</h2>
+                            <p>McDoolans</p>
+                        </li>
+                        <li>
+                            <h2>Statistics</h2>
+                            <p>Total Reviews: </p>
+                            <p>Favorite Places: </p>
+                        </li>
+                        <li>
+                            <h2>Socials</h2>
+                            <p>LinkedIn</p>
+                            <p>FaceBook</p>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
