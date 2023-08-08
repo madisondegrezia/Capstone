cf82788cf1e3c6773ec2d9e4c1e4a085cc61cc6a
tonglin <tl82003@gmail.com>
2023-08-08 00:35:30 -0400

    fixed problem not getting when restaurant logged

--- frontend/social-bites/src/RestaurantPage/RestaurantPage.jsx
+++ frontend/social-bites/src/RestaurantPage/RestaurantPage.jsx
@@ -34,78 +34,37 @@ export default function RestaurantPage() {
 
   const location = useLocation();
 
-
   return (
     <>
       {restaurant ? (
-        <div className="main-wrap">
-          <div className="wrapper1">
-            <div className="restaurant-page">
-              <img
-                className="res-hero"
-                //src="https://www.deputy.com/uploads/2018/10/The-Most-Popular-Menu-Items-That-You-should-Consider-Adding-to-Your-Restaurant_Content-image3-min-1024x569.png"
-                src={restaurant.heroImage}
-              ></img>
-
-              <div className="res-content"></div>
+        <div>
+          <div className="main-wrap">
+            <div className="wrapper1">
+              <div className="restaurant-page">
+                <img
+                  className="res-hero"
+                  src={restaurant.heroImage}
+                  alt="Restaurant Hero"
+                />
+                <div className="res-content"></div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="wrapper flex flex-row w-screen">
-          <nav id="sidebar">
-            <img
-              className="profile-image"
-              src="https://www.auntminnie.com/user/images/content_images/nws_rad/2015_01_28_12_24_19_220_hamburger_200.jpg"
-            ></img>
-
-              <ul className="list-unstyled components">
-                <p className="res-name">{restaurant.restaurantName}</p>
-                <li className="flex flex-row items-center justify-start px-5 py-5">
-                  <div className="icons">
-                    <MdStarRate size={25} />
-                  </div>
-                  <a href="#">Rating</a>
-                </li>
-                <li className="flex flex-row items-center justify-start px-5 py-5">
-                  <div className="icons">
-                    <MdOutlineRateReview size={25} />
-                  </div>
-                  <a href="restaurant/reviews">Reviews</a>
-                </li>
-                <li className="flex flex-row items-center justify-start px-5 py-5">
-                  <div className="icons">
-                    <MdOutlineRestaurantMenu size={25} />
-                  </div>
-                  <a href="#">Menu</a>
-                </li>
-                <li className="flex flex-row items-center justify-start px-5 py-5">
-                  <div className="icons">
-                    <GrContact size={25} />
-                  </div>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-
-              <ul className="list-unstyled CTAs">
-                <li>
-                  <a href="/" className="article">
-                    Back to home
-                  </a>
-                </li>
-              </ul>
+          <div className="wrapper flex flex-row w-screen">
+            <nav id="sidebar">
+              {/* ... */}
             </nav>
-
-          <div id="contentz">
-            
-            <Outlet />
-          </div>
-
-          {location.pathname == "/restaurant/:id" ? <div id="suggestion-content">
-            <h2 className="text-3xl suggestions">
-              More Restaurants like Bob's Burgers
-            </h2>
-            {/* <RestaurantCards /> */}
-          </div> : null}
+            <div id="contentz">
+              <Outlet />
+            </div>
+            {location.pathname === `/restaurant/${restaurantId}` ? (
+              <div id="suggestion-content">
+                <h2 className="text-3xl suggestions">
+                  More Restaurants like Bob's Burgers
+                </h2>
+                {/* <RestaurantCards /> */}
+              </div>
+            ) : null}
           </div>
         </div>
       ) : (
